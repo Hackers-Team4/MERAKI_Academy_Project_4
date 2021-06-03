fdc3199217b9429c399abbe4465f45eb94463e56
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-03 11:56:20 +0300

    update user

--- backend/routers/routes/user.js
+++ backend/routers/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 
-const { createNewUser } = require('./../controllers/user');
+const { createNewUser, updateUser } = require('./../controllers/user');
 
 const usersRouter = express.Router();
 
 usersRouter.post('/register', createNewUser);
+usersRouter.put('/user_updat/:id',updateUser)
 
 module.exports = usersRouter;

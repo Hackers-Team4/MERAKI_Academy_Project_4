00474f64b0d001aec1b9c32628f5d585a405eb4c
yazanmo <yazanmostafa96@gmail.com>
2021-06-06 11:40:20 +0300

    work on login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Navigation from './components/navigation';
-import Register from './components/signIn/signUp'
+import Register from './components/signIn/signUp';
+import Login from './components/signIn/login';
 
 const App = () => {
 	return <div className="App">
 		<Navigation/>
 		<Route exact path="/singIn" component = {Register}/>
+		<Route exact path="/logIn" component = {Login}/>
+
 
 
 	</div>;

f616143de9ee1fbeb9ecbfa3dfcc779a2343024d
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-07 17:54:14 +0300

    creat abi by typeOfFood

--- backend/routers/routes/restaurant.js
+++ backend/routers/routes/restaurant.js
@@ -1,22 +1,24 @@
 const express = require('express');
-const {createRestaurant ,
-       getAllRestaurant,
-       updateRestaurantById,
-       deleteRestaurantById,
-       getRestaurantById
-     } = require ("./../controllers/restaurant")
+const { createRestaurant,
+  getAllRestaurant,
+  updateRestaurantById,
+  deleteRestaurantById,
+  getRestaurantById,
+  getRestaurantByTypeOfFood
+} = require("./../controllers/restaurant")
 
 
 
 
-  
-
- restaurantRouter =express.Router();
- restaurantRouter.post("/restaurants",createRestaurant )
- restaurantRouter.get("/restaurants",getAllRestaurant)
- restaurantRouter.get("/restaurants/:id",getRestaurantById)
- restaurantRouter.put("/restaurants_updated/:id",updateRestaurantById)
- restaurantRouter.delete("/restaurants_delete/:id",deleteRestaurantById)
 
 
- module.exports= restaurantRouter;
+restaurantRouter = express.Router();
+restaurantRouter.post("/restaurants", createRestaurant)
+restaurantRouter.get("/restaurants", getAllRestaurant)
+restaurantRouter.get("/restaurants/:id", getRestaurantById)
+restaurantRouter.get("/restaurants/search_1",getRestaurantByTypeOfFood)
+restaurantRouter.put("/restaurants_updated/:id", updateRestaurantById)
+restaurantRouter.delete("/restaurants_delete/:id", deleteRestaurantById)
+
+
+module.exports = restaurantRouter;

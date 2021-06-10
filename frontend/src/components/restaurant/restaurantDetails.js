63c30c7988445169d0b3b4879f0c89d128d5faaf
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-11 02:44:56 +0300

    work in restaurant details part_2

--- frontend/src/components/restaurant/restaurantDetails.js
+++ frontend/src/components/restaurant/restaurantDetails.js
@@ -56,7 +56,7 @@ const RestaurantDetails = (props) => {
       <div className="RN">
         <img src={`${restaurant.images && restaurant.images[0]}`} className="firstImg" />
         <div className="RInfo">
-          <p style={{fontSize:"50px"}}>{restaurant.restaurantName}</p>
+          <p style={{ fontSize: "35px" }}>{restaurant.restaurantName}</p>
           <div className="loc_type">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
               <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
@@ -67,24 +67,18 @@ const RestaurantDetails = (props) => {
               <path d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z" />
             </svg>
             <p>{restaurant.typeOfFood}</p>
+
+            <button onClick={goToBook} className="btnBooking">Booking</button>
+
           </div>
         </div>
       </div>
-      <p>{restaurant.description}</p>
-      <button onClick={goToBook}>Booking</button>
 
-      <SRLWrapper>
-        {" "}
-        {restaurant.images &&
-          restaurant.images.map((elem, i) => {
-            return (
-              <a href={`${elem}`}>
-                <img src={`${elem}`} />
-              </a>
-            );
-          })}
-      </SRLWrapper>
-      <button onClick={() => setShow(!Show)}>menu</button>
+      <div className="description">
+        {/* <p style={{fontSize:"30px"}}>{restaurant.restaurantName}</p> */}
+        <p style={{ marginTop: "10px" }}>{restaurant.description}</p>
+      </div>
+      <button className="btnMenu" onClick={() => setShow(!Show)}>menu</button>
 
       {Show ? (
         <p>
@@ -95,6 +89,27 @@ const RestaurantDetails = (props) => {
           </p>
         </p>
       ) : null}
+      <SRLWrapper>
+        {" "}
+        {/* {restaurant.images &&
+          restaurant.images.map((elem, i) => {
+            return (
+              <a href={`${elem}`}>
+                <img src={`${elem}`} />
+              </a>
+            );
+          })} */}
+        <div className="imageSliderP">
+          <img src={`${restaurant.images && restaurant.images[0]}`} style={{ width: "700px", height: "400px",borderRadius:"5px"}} />
+          <div className="imageSliderC">
+            <img  src={`${restaurant.images && restaurant.images[1]}`} style={{ width: "172px", height: "172px" ,borderRadius:"5px"}} />
+            <img src={`${restaurant.images && restaurant.images[2]}`} style={{ width: "172px", height: "172px" ,borderRadius:"5px"}}/>
+            <img src={`${restaurant.images && restaurant.images[3]}`} style={{ width: "172px", height: "172px",borderRadius:"5px" }} />
+            <img src={`${restaurant.images && restaurant.images[4]}`} style={{ width: "172px", height: "172px" ,borderRadius:"5px"}} />
+          </div>
+        </div>
+      </SRLWrapper>
+
 
       {/* <p id="ff">menu 1111111111111111111111111111111</p> */}
 

7623b5c6105ee46cd82838fd42e9fe66bd3d446b
yazanmo <yazanmostafa96@gmail.com>
2021-06-12 01:40:04 +0300

    css booking done

--- frontend/src/components/restaurant/Booking.js
+++ frontend/src/components/restaurant/Booking.js
@@ -21,7 +21,7 @@ const Booking = (props) => {
 
                     <div className="inforesturant">
                         <p >  {props.book2.images && <img className="imageres" src={`${props.book2.images[0]}`} />}</p>
-                        <p className="resname"> resturant name : {props.book2.restaurantName && props.book2.restaurantName}</p>
+                        <p className="resname">{props.book2.restaurantName && props.book2.restaurantName}</p>
                     </div>
 
 
@@ -41,6 +41,17 @@ const Booking = (props) => {
                             <option value="3 people">3 people</option>
                             <option value="4 people">4 people</option>
                             <option value="5 people">5 people</option>
+                            <option value="5 people">6 people</option>
+                            <option value="5 people">7 people</option>
+                            <option value="5 people">8 people</option>
+                            <option value="5 people">9 people</option>
+                            <option value="5 people">10 people</option>
+                            <option value="5 people">11 people</option>
+                            <option value="5 people">12 people</option>
+                            <option value="5 people">13 people</option>
+                            <option value="5 people">14 people</option>
+                            <option value="5 people">15 people</option>
+
                         </select>
                         </div>
 
@@ -61,10 +72,11 @@ const Booking = (props) => {
 
            {card? <div className="cardinfo">
                 <div>
-                <p > your name: {props.userB.firstName} <span>{props.userB.LastName}</span> </p>
-                <p >phome number:{props.userB.phoneNumber}</p>
-                <p>The reservation was successfully booked on <span>{date}</span> </p>
-                <p> at <span>{time}</span> for the number of people <span>{people}</span> </p>
+                    <br></br>
+                <p > Your name : {props.userB.firstName} <span>{props.userB.LastName}</span> </p>
+                <p >Phome number : {props.userB.phoneNumber}</p>
+                <p> The reservation was successfully booked on <span>{date}</span> </p>
+                <p> At <span>{time}</span> for The number of people <span>{people}</span> </p>
                 </div>
             
             </div>:""}

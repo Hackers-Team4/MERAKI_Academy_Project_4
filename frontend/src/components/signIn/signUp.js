7086070bd2aeac3aaec1ac652bf5f9816fa6528e
MohammadShoman <mohammad.shoman25@gmail.com>
2021-06-12 03:28:43 -0700

    style for sign up

--- frontend/src/components/signIn/signUp.js
+++ frontend/src/components/signIn/signUp.js
@@ -46,8 +46,15 @@ const Register = ({ setUsers }) => {
   }
 
   return (
-    <>
+    <><div className="signUpImg">
+    <svg style={{marginTop:"30px"}} xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+<path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+<path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+</svg>
+<h1>SIGN UP</h1>
+</div>
       <div className="allInput">
+        
         <input className="inputs" type="text" placeholder="firstName here" onChange={(e) => {
           setfirstName(e.target.value)
         }} />
@@ -78,11 +85,12 @@ const Register = ({ setUsers }) => {
           setpassword(e.target.value)
         }} />
 
-        <din className="singUp"><p>You already have an account ?<span><Link to="/login"> log in</Link></span></p> </din>
 
 
       </div>
       <div className="btn_reg"><button className="btn" onClick={cheakRegiste}>Register</button>
+      <din className="singUp" style={{marginTop:"20px"}}><p>You already have an account ?<span><Link to="/login"> log in</Link></span></p> </din>
+
         {/*<div><p>You already have an account ?<span><Link to="/login"> log in</Link></span></p></div>*/}
         {stata ? <div className="trueRegiste">The user has been created successfully</div> : ""}
         {stata1 ? <div className="falseRegiste">Error happened while register, please try again</div> : ""}</div>
